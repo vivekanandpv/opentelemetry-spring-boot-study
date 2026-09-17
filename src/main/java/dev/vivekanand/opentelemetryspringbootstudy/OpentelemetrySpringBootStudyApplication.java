757a082db0674f1afbc468313a77de6a22181291
Vivekanand Pujar <vivek@vivekanand.dev>
2026-09-17 18:35:53 +0530

    Initial commit

---
+++ src/main/java/dev/vivekanand/opentelemetryspringbootstudy/OpentelemetrySpringBootStudyApplication.java
@@ -0,0 +1,13 @@
+package dev.vivekanand.opentelemetryspringbootstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpentelemetrySpringBootStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpentelemetrySpringBootStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/vivekanand/opentelemetryspringbootstudy/OpentelemetrySpringBootStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vivekanand.opentelemetryspringbootstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpentelemetrySpringBootStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
